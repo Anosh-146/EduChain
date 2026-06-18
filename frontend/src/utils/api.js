ef438e21a1020151d2d854cac4138ab4917a2914
Anosh-146 <23501a4210@pvpsit.ac.in>
2026-06-18 16:30:53 +0530

    Update API base URL to production endpoint

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://educhain-814d.onrender.com/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
